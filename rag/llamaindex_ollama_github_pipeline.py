8c642bfa0294df80a1ebb62af404fdbe9f3adae6
David Sewell <206408+dsewell@users.noreply.github.com>
2025-02-23 12:09:57 +0000

    dependencies in github pipeline

--- rag/llamaindex_ollama_github_pipeline.py
+++ rag/llamaindex_ollama_github_pipeline.py
@@ -5,7 +5,7 @@ date: 2024-05-30
 version: 1.0
 license: MIT
 description: A pipeline for retrieving relevant information from a knowledge base using the Llama Index library with Ollama embeddings from a GitHub repository.
-requirements: llama-index, llama-index-llms-ollama, llama-index-embeddings-ollama, llama-index-readers-github
+requirements: llama-index-core>=0.12.19, llama-index, llama-index-llms-ollama, llama-index-embeddings-ollama, llama-index-readers-github
 """
 
 from typing import List, Union, Generator, Iterator
